1fb0aab384d8270e0f26ca081a3c648e6878d8e4
Andrew Walker <awalker@ixsystems.com>
2021-03-25 12:23:19 -0400

    Fix compiler warning

--- fs/attr.c
+++ fs/attr.c
@@ -28,16 +28,6 @@ static bool chown_ok(const struct inode *inode, kuid_t uid)
 	if (uid_eq(inode->i_uid, INVALID_UID) &&
 	    ns_capable(inode->i_sb->s_user_ns, CAP_CHOWN))
 		return true;
-#ifdef CONFIG_TRUENAS
-	/*
-	 * Check for ACE4_WRITE_OWNER. RFC 5661 Section 6.2.1.3.1
-	 * On UNIX systems, this is the ability to execute chown() and
-	 * chgrp().
-	 */
-	if (IS_NFSV4ACL(inode) &&
-	    (inode_permission(inode, MAY_WRITE_OWNER) == 0))
-		return true;
-#endif
 	return false;
 }
 
@@ -51,16 +41,6 @@ static bool chgrp_ok(const struct inode *inode, kgid_t gid)
 	if (gid_eq(inode->i_gid, INVALID_GID) &&
 	    ns_capable(inode->i_sb->s_user_ns, CAP_CHOWN))
 		return true;
-#ifdef CONFIG_TRUENAS
-	/*
-	 * Check for ACE4_WRITE_OWNER. RFC 5661 Section 6.2.1.3.1
-	 * On UNIX systems, this is the ability to execute chown() and
-	 * chgrp().
-	 */
-	if (IS_NFSV4ACL(inode) &&
-	    (inode_permission(inode, MAY_WRITE_OWNER) == 0))
-		return true;
-#endif
 	return false;
 }
 
@@ -98,12 +78,39 @@ int setattr_prepare(struct dentry *dentry, struct iattr *attr)
 		goto kill_priv;
 
 	/* Make sure a caller can chown. */
+#if CONFIG_TRUENAS
+	/*
+	 * Check for ACE4_WRITE_OWNER. RFC 5661 Section 6.2.1.3.1
+	 * On UNIX systems, this is the ability to execute chown() and
+	 * chgrp().
+	 */
+	if ((ia_valid & ATTR_UID) && !chown_ok(inode, attr->ia_uid)) {
+		if (!IS_NFSV4ACL(inode)) {
+			return -EPERM;
+		}
+		else if (inode_permission(inode, MAY_WRITE_OWNER) != 0) {
+			return -EPERM;
+		}
+	}
+#else
 	if ((ia_valid & ATTR_UID) && !chown_ok(inode, attr->ia_uid))
 		return -EPERM;
+#endif
 
 	/* Make sure caller can chgrp. */
+#if CONFIG_TRUENAS
+	if ((ia_valid & ATTR_GID) && !chgrp_ok(inode, attr->ia_gid)) {
+		if (!IS_NFSV4ACL(inode)) {
+			return -EPERM;
+		}
+		else if (inode_permission(inode, MAY_WRITE_OWNER) != 0) {
+			return -EPERM;
+		}
+	}
+#else
 	if ((ia_valid & ATTR_GID) && !chgrp_ok(inode, attr->ia_gid))
 		return -EPERM;
+#endif
 
 	/* Make sure a caller can chmod. */
 	if (ia_valid & ATTR_MODE) {
