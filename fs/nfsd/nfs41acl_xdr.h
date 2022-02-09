709d09683bdf92b406a51497fd821bf9619c49c9
Andrew <awalker@ixsystems.com>
2022-02-09 11:30:47 -0600

    Implement native NFSv4 ACLs in NFS server (#17)
    ZFS currently exposes native ZFS nfsv4 ACL type through
    the system.nfs4_acl_xdr xattr. underlying ACL type can
    be determined via inode sb flags (previous kernel work).
    
    This PR makes the Kernel NFS server aware of this ACL
    type and has it read from and write to the xattr is
    the relevant NFS40 ops.
    
    At this point only NFS40 ACLs are implemented (RFC3530)
    because they are currently only ones supported by the
    Kernel NFS client in Linux.
    
    ACE_INHERITED_ACE is returned in request for NFS40 ACL.
    This is for consistency with FreeBSD behavior, and is
    important for SMB / NFS compatibility. At a future
    point in time, full NFS41 ACL compatibility will
    most likely be required (for feature parity with
    other commercial vendors). nfs4-acl-tools version 0.3.3
    and earlier contains a bug where the persence of
    ACE_INHERITED_ACE is mistakenly identified as making
    ACE apply to the file owner.
    
    Signed-off-by: Andrew Walker <awalker@ixsystems.com>

---
+++ fs/nfsd/nfs41acl_xdr.h
@@ -0,0 +1,61 @@
+#ifndef _NFS41ACL_H_RPCGEN
+#define _NFS41ACL_H_RPCGEN
+
+#define NA41_NAME "system.nfs4_acl_xdr"
+
+typedef u_int acetype4;
+#define ACE4_FILE_INHERIT_ACE 0x00000001
+#define ACE4_DIRECTORY_INHERIT_ACE 0x00000002
+#define ACE4_NO_PROPAGATE_INHERIT_ACE 0x00000004
+#define ACE4_INHERIT_ONLY_ACE 0x00000008
+#define ACE4_SUCCESSFUL_ACCESS_ACE_FLAG 0x00000010
+#define ACE4_FAILED_ACCESS_ACE_FLAG 0x00000020
+#define ACE4_IDENTIFIER_GROUP 0x00000040
+#define ACE4_INHERITED_ACE 0x00000080
+#define NFS41_FLAGS	(ACE4_DIRECTORY_INHERIT_ACE| \
+			 ACE4_FILE_INHERIT_ACE| \
+			 ACE4_NO_PROPAGATE_INHERIT_ACE| \
+			 ACE4_INHERIT_ONLY_ACE| \
+			 ACE4_INHERITED_ACE| \
+			 ACE4_IDENTIFIER_GROUP)
+
+typedef u_int aceflag4;
+#define ACEI4_SPECIAL_WHO 0x00000001
+
+typedef u_int aceiflag4;
+#define ACE4_SPECIAL_OWNER 1
+#define ACE4_SPECIAL_GROUP 2
+#define ACE4_SPECIAL_EVERYONE 3
+
+typedef u_int acemask4;
+
+struct nfsace4i {
+	acetype4 type;
+	aceflag4 flag;
+	aceiflag4 iflag;
+	acemask4 access_mask;
+	u_int who;
+};
+typedef struct nfsace4i nfsace4i;
+#define NACE41_LEN 5
+#define ACL4_AUTO_INHERIT 0x00000001
+#define ACL4_PROTECTED 0x00000002
+#define ACL4_DEFAULTED 0x00000004
+
+typedef u_int aclflag4;
+
+/*
+ * Macros for sanity checks related to XDR and ACL buffer sizes
+ */
+#define NFS41ACL_MAX_ENTRIES	128
+#define ACE4SIZE                (sizeof (nfsace4i))
+#define XDRBASE                 (2 * sizeof (u32))
+
+#define ACES_TO_SIZE(x, y)      (x + (y * ACE4SIZE))
+#define SIZE_IS_VALID(x, y)     ((x >= ACES_TO_SIZE(y, 0)) && \
+                                (((x - y) % ACE4SIZE) == 0))
+
+#define ACES_TO_XDRSIZE(x)      (ACES_TO_SIZE(XDRBASE, x))
+#define XDRSIZE_IS_VALID(x)     (SIZE_IS_VALID(x, XDRBASE))
+
+#endif /* !_NFS41ACL_H_RPCGEN */
