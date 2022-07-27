3365b08ac6c6a90e88f5dba22668216d2d432bdd
Alexander Motin <mav@ixsystems.com>
2022-07-27 17:22:16 -0400

    Map only B2B BAR on attach, not all of them.
    The driver itself needs only configuration and B2B BARs, and both
    of them must be mapped uncached.  Use of other BARs and their caching
    is up to consumers.  Mapping them here is both unneeded and harmful,
    since consumers may be unable to remap them with less strict semantics.
    
    While here, remove pcim_iounmap() calls on deinit, since kernel can do
    it for us automatically on driver detach.

--- drivers/ntb/hw/plx/ntb_hw_plx.c
+++ drivers/ntb/hw/plx/ntb_hw_plx.c
@@ -44,7 +44,6 @@ static struct dentry *debugfs_dir;
 struct plx_ntb_mw_info {
 	int		mw_bar;
 	int		mw_64bit;
-	void __iomem	*mw_res;
 	dma_addr_t	mw_pbase;
 	resource_size_t	mw_size;
 	struct {
@@ -81,6 +80,7 @@ struct plx_ntb_dev {
 	/* Parameters of window shared with peer config access in B2B mode. */
 	unsigned int b2b_mw;	/* Shared window number. */
 	u64 b2b_off;	/* Offset in shared window. */
+	void __iomem *b2b_mmio;
 };
 
 #define NTB_LNK_STA_SPEED_MASK	0x000F
@@ -140,7 +140,7 @@ struct plx_ntb_dev {
 #define PEER_BASE(n)			((n)->self_mmio + PLX_NTX_PEER_BASE(n))
 
 /* B2B NTx registers */
-#define B2B_BASE(n)			((n)->mw_info[(n)->b2b_mw].mw_res)
+#define B2B_BASE(n)			((n)->b2b_mmio)
 #define B2B_REG(n, reg)			(PLX_NTX_BASE(n) + (reg))
 
 #define PLX_PORT_BASE(p)		((p) << 12)
@@ -212,14 +212,6 @@ err_pci_enable:
 static void plx_ntb_deinit_pci(struct plx_ntb_dev *ndev)
 {
 	struct pci_dev *pdev = ndev->ntb.pdev;
-	struct plx_ntb_mw_info *mw;
-	int i;
-
-	for (i = 0; i < ndev->mw_count; i++) {
-		mw = &ndev->mw_info[i];
-		pcim_iounmap(pdev, mw->mw_res);
-	}
-	pcim_iounmap(pdev, ndev->self_mmio);
 
 	pci_clear_master(pdev);
 	pci_release_regions(pdev);
@@ -678,9 +670,6 @@ static int plx_init_dev(struct ntb_dev *ntb)
 	for (i = 2; i <= 5; i++) {
 		mw = &ndev->mw_info[ndev->mw_count];
 		mw->mw_bar = i;
-		mw->mw_res = pcim_iomap(pdev, mw->mw_bar, 0);
-		if (!mw->mw_res)
-			continue;
 		mw->mw_pbase = pci_resource_start(pdev, mw->mw_bar);
 		mw->mw_size = pci_resource_len(pdev, mw->mw_bar);
 		ndev->mw_count++;
@@ -723,6 +712,14 @@ static int plx_init_dev(struct ntb_dev *ntb)
 			ndev->b2b_off = mw->mw_size / 2;
 		else
 			ndev->b2b_off = 0;
+
+		ndev->b2b_mmio = pcim_iomap(pdev, mw->mw_bar,
+		    mw->mw_size - ndev->b2b_off);
+		if (!ndev->b2b_mmio) {
+			dev_info(&pdev->dev, "Can't map B2B BAR.\n");
+			rc = -ENOMEM;
+			goto out;
+		}
 	}
 
 	/* Check the module parameter for user defined split value, default is 0 if
@@ -863,7 +860,7 @@ static int plx_ntb_peer_spad_write(struct ntb_dev *ntb, int pidx, int sidx, u32 
 	else
 		offset = ndev->pspad_off2 + (sidx - ndev->spad_cnt1) * 4;
 	if (ndev->b2b_mw >= 0)
-		writel(val, ndev->mw_info[ndev->b2b_mw].mw_res + offset);
+		writel(val, ndev->b2b_mmio + offset);
 	else
 		writel(val, ndev->self_mmio + offset);
 
@@ -883,7 +880,7 @@ static u32 plx_ntb_peer_spad_read(struct ntb_dev *ntb, int pidx, int sidx)
 	else
 		offset = ndev->pspad_off2 + (sidx - ndev->spad_cnt1) * 4;
 	if (ndev->b2b_mw >= 0)
-		return readl(ndev->mw_info[ndev->b2b_mw].mw_res + offset);
+		return readl(ndev->b2b_mmio + offset);
 	else
 		return readl(ndev->self_mmio + offset);
 }
