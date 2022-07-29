5f328ff9367d05a55775fd42a55d3c8e55816ae5
Alexander Motin <mav@ixsystems.com>
2022-07-29 12:10:50 -0400

    Introduce NVDIMM NTB mirroring driver.
    It is a fully compatible port of respective functionality from FreeBSD
    TrueNAS Core.  On Linux it handles only NVDIMMs without labels, i.e.
    only "raw" namespaces, covering whole range.  Instead the driver writes
    own label into the last page of NVDIMM, using it to store disk serial
    number and some information required for data synchronization.

--- drivers/nvdimm/pmem.c
+++ drivers/nvdimm/pmem.c
@@ -46,6 +46,36 @@ static struct nd_region *to_region(struct pmem_device *pmem)
 	return to_nd_region(to_dev(pmem)->parent);
 }
 
+static int pmem_open(struct gendisk *disk, blk_mode_t mode)
+{
+	struct pmem_device *pmem = disk->private_data;
+	struct pmem_label *label = pmem->label;
+
+	++pmem->opened_all;
+
+	if (mode & BLK_OPEN_WRITE)
+		pmem->opened_for_write = true;
+
+	if (label && pmem->opened_for_write)
+		label->opened = pmem->opened_all;
+
+	return 0;
+}
+
+static void pmem_release(struct gendisk *disk)
+{
+	struct pmem_device *pmem = disk->private_data;
+	struct pmem_label *label = pmem->label;
+
+	--pmem->opened_all;
+
+	if (label && pmem->opened_for_write)
+		label->opened = pmem->opened_all;
+
+	if (pmem->opened_all == 0)
+		pmem->opened_for_write = false;
+}
+
 static phys_addr_t pmem_to_phys(struct pmem_device *pmem, phys_addr_t offset)
 {
 	return pmem->phys_addr + offset;
@@ -122,6 +152,26 @@ static blk_status_t pmem_clear_poison(struct pmem_device *pmem,
 	return BLK_STS_OK;
 }
 
+static void write_pmem2(void *pmem_addr, void *pmem_addr2, struct page *page,
+		unsigned int off, unsigned int len)
+{
+	unsigned int chunk;
+	void *mem;
+
+	while (len) {
+		mem = kmap_atomic(page);
+		chunk = min_t(unsigned int, len, PAGE_SIZE - off);
+		memcpy_flushcache(pmem_addr, mem + off, chunk);
+		memcpy(pmem_addr2, mem + off, chunk);
+		kunmap_atomic(mem);
+		len -= chunk;
+		off = 0;
+		page++;
+		pmem_addr += chunk;
+		pmem_addr2 += chunk;
+	}
+}
+
 static void write_pmem(void *pmem_addr, struct page *page,
 		unsigned int off, unsigned int len)
 {
@@ -184,6 +234,21 @@ static blk_status_t pmem_do_write(struct pmem_device *pmem,
 {
 	phys_addr_t pmem_off = to_offset(pmem, sector);
 	void *pmem_addr = pmem->virt_addr + pmem_off;
+	struct pmem_label *label = pmem->label;
+	struct pmem_label *rlabel = pmem->rlabel;
+	void *rpmem_addr = pmem->rvirt_addr;
+
+	if (label) {
+		if (unlikely(label->empty)) {
+			label->empty = 0;
+			if (rlabel)
+				rlabel->empty = 0;
+		}
+		if (!rpmem_addr && unlikely(!label->dirty)) {
+			label->dirty = 1;
+			arch_wb_cache_pmem(label, sizeof(struct pmem_label));
+		}
+	}
 
 	if (unlikely(is_bad_pmem(&pmem->bb, sector, len))) {
 		blk_status_t rc = pmem_clear_poison(pmem, pmem_off, len);
@@ -193,7 +258,11 @@ static blk_status_t pmem_do_write(struct pmem_device *pmem,
 	}
 
 	flush_dcache_page(page);
-	write_pmem(pmem_addr, page, page_off, len);
+	if (rpmem_addr) {
+		write_pmem2(pmem_addr, rpmem_addr + pmem_off, page, page_off,
+		    len);
+	} else
+		write_pmem(pmem_addr, page, page_off, len);
 
 	return BLK_STS_OK;
 }
@@ -288,6 +357,8 @@ __weak long __pmem_direct_access(struct pmem_device *pmem, pgoff_t pgoff,
 
 static const struct block_device_operations pmem_fops = {
 	.owner =		THIS_MODULE,
+	.open =			pmem_open,
+	.release =		pmem_release,
 	.submit_bio =		pmem_submit_bio,
 };
 
@@ -371,6 +442,53 @@ static const struct dax_operations pmem_dax_ops = {
 	.recovery_write = pmem_recovery_write,
 };
 
+static ssize_t label_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct pmem_device *pmem = dev_to_disk(dev)->private_data;
+	struct pmem_label *label = pmem->label;
+
+	if (label) {
+		memcpy(buf, label, sizeof(*label));
+		return sizeof(*label);
+	}
+	return -ENXIO;
+}
+static DEVICE_ATTR_RO(label);
+
+static ssize_t uuid_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct pmem_device *pmem = dev_to_disk(dev)->private_data;
+	struct pmem_label *label = pmem->label;
+
+	if (label)
+		return sprintf(buf, "%016llX\n", label->array);
+	return -ENXIO;
+}
+static DEVICE_ATTR_RO(uuid);
+
+static struct attribute *label_attributes[] = {
+	&dev_attr_label.attr,
+	&dev_attr_uuid.attr,
+	NULL,
+};
+
+static umode_t label_visible(struct kobject *kobj, struct attribute *a, int n)
+{
+	struct device *dev = container_of(kobj, typeof(*dev), kobj);
+	struct pmem_device *pmem = dev_to_disk(dev)->private_data;
+
+	if (!pmem->label)
+		return 0;
+	return a->mode;
+}
+
+static const struct attribute_group label_attribute_group = {
+	.attrs		= label_attributes,
+	.is_visible	= label_visible,
+};
+
 static ssize_t write_cache_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -415,6 +533,7 @@ static const struct attribute_group dax_attribute_group = {
 };
 
 static const struct attribute_group *pmem_attribute_groups[] = {
+	&label_attribute_group,
 	&dax_attribute_group,
 	NULL,
 };
@@ -547,8 +666,18 @@ static int pmem_attach_disk(struct device *dev,
 	}
 	pmem->virt_addr = addr;
 
+	/* Register raw pmems for NTB mirroring. */
+	if (!is_nd_pfn(dev) && !is_nd_dax(dev) &&
+	    uuid_is_null((const uuid_t *)nd_dev_to_uuid(&ndns->dev))) {
+		pmem->bb.dev = dev;
+		ntb_pmem_register(pmem);
+	}
+
+
 	disk->fops		= &pmem_fops;
 	disk->private_data	= pmem;
+	disk->events		= DISK_EVENT_MEDIA_CHANGE;
+	disk->event_flags	= DISK_EVENT_FLAG_UEVENT;
 	nvdimm_namespace_disk_name(ndns, disk->disk_name);
 	set_capacity(disk, (pmem->size - pmem->pfn_pad - pmem->data_offset)
 			/ 512);
@@ -656,6 +785,10 @@ static void nd_pmem_remove(struct device *dev)
 	if (is_nd_btt(dev))
 		nvdimm_namespace_detach_btt(to_nd_btt(dev));
 	else {
+		/* Unregister from NTB mirroring. */
+		if (pmem->label)
+			ntb_pmem_unregister(pmem);
+
 		/*
 		 * Note, this assumes device_lock() context to not
 		 * race nd_pmem_notify()
--- drivers/nvdimm/pmem.h
+++ drivers/nvdimm/pmem.h
@@ -8,6 +8,27 @@
 #include <linux/pfn_t.h>
 #include <linux/fs.h>
 
+#define PMEM_SIGN_SHORT	0x4e564430
+#define PMEM_SIGN_LONG	0x4e5644494d4d3030
+
+enum {
+	STATE_INCORRECT = 0,
+	STATE_NONE,
+	STATE_IDLE,
+	STATE_WAITING,
+	STATE_READY,
+};
+
+/* PMEM label */
+struct pmem_label {
+	u64	sign;		/* PMEM_SIGN_LONG signature */
+	u64	array;		/* Unique array ID */
+	u32	empty;		/* PMEM is empty and was never written */
+	u32	dirty;		/* PMEM was written without NTB connection */
+	u32	opened;		/* PMEM device is open now */
+	u32	state;		/* Synchronization state */
+};
+
 enum dax_access_mode;
 
 /* this definition is in it's own header for tools/testing/nvdimm to consume */
@@ -27,6 +48,14 @@ struct pmem_device {
 	struct dax_device	*dax_dev;
 	struct gendisk		*disk;
 	struct dev_pagemap	pgmap;
+
+	struct pmem_label	*label;		/* Local PMEM label */
+	phys_addr_t		 rphys_addr;	/* Remote PMEM phys address */
+	uint8_t			*rvirt_addr;	/* Remote PMEM KVA address */
+	struct pmem_label	*rlabel;	/* Remote PMEM label */
+	int			 rnode;		/* Remote PMEM (NTB) node */
+	bool			 opened_for_write;	/* Device was opened with BLK_OPEN_WRITE */
+	int			 opened_all;	/* Track all device opens */
 };
 
 long __pmem_direct_access(struct pmem_device *pmem, pgoff_t pgoff,
@@ -44,4 +73,7 @@ static inline bool test_and_clear_pmem_poison(struct page *page)
 	return false;
 }
 #endif
+
+void ntb_pmem_register(struct pmem_device *pdev);
+void ntb_pmem_unregister(struct pmem_device *pdev);
 #endif /* __NVDIMM_PMEM_H__ */
