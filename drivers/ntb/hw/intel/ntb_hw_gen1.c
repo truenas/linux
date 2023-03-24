d974967a765c597f636678f2550b80ecfd18c29e
Alexander Motin <mav@ixsystems.com>
2023-03-24 14:07:21 -0400

    ntb: intel: Set proper DMA masks, matching XLAT sizes.
    Without this with BAR split activated ntb_transport allocates memory
    that can't be handled by the 32bit XLAT registers.
    
    Signed-off-by: Alexander Motin <mav@ixsystems.com>

--- drivers/ntb/hw/intel/ntb_hw_gen1.c
+++ drivers/ntb/hw/intel/ntb_hw_gen1.c
@@ -1767,16 +1767,6 @@ static int intel_ntb_init_pci(struct intel_ntb_dev *ndev, struct pci_dev *pdev)
 	if (rc)
 		goto err_pci_regions;
 
-	pci_set_master(pdev);
-
-	rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(64));
-	if (rc) {
-		rc = dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
-		if (rc)
-			goto err_dma_mask;
-		dev_warn(&pdev->dev, "Cannot DMA highmem\n");
-	}
-
 	ndev->self_mmio = pci_iomap(pdev, 0, 0);
 	if (!ndev->self_mmio) {
 		rc = -EIO;
@@ -1794,6 +1784,33 @@ err_pci_regions:
 	pci_disable_device(pdev);
 err_pci_enable:
 	pci_set_drvdata(pdev, NULL);
+	return rc;
+}
+
+static int intel_ntb_init_dma(struct intel_ntb_dev *ndev, struct pci_dev *pdev)
+{
+	int rc;
+
+	pci_set_master(pdev);
+
+	rc = dma_set_mask(&pdev->dev, DMA_BIT_MASK(64));
+	if (rc)
+		rc = dma_set_mask(&pdev->dev, DMA_BIT_MASK(32));
+	if (rc)
+		goto err_dma_mask;
+
+	rc = -EIO;
+	if (!ndev->bar4_split)
+		rc = dma_set_coherent_mask(&pdev->dev, DMA_BIT_MASK(64));
+	if (rc)
+		rc = dma_set_coherent_mask(&pdev->dev, DMA_BIT_MASK(32));
+	if (rc)
+		goto err_dma_mask;
+
+	return 0;
+
+err_dma_mask:
+	pci_clear_master(pdev);
 	return rc;
 }
 
@@ -1886,6 +1903,10 @@ static int intel_ntb_pci_probe(struct pci_dev *pdev,
 
 	ndev_reset_unsafe_flags(ndev);
 
+	rc = intel_ntb_init_dma(ndev, pdev);
+	if (rc)
+		goto err_init_dma;
+
 	ndev->reg->poll_link(ndev);
 
 	ndev_init_debugfs(ndev);
@@ -1900,6 +1921,7 @@ static int intel_ntb_pci_probe(struct pci_dev *pdev,
 
 err_register:
 	ndev_deinit_debugfs(ndev);
+err_init_dma:
 	if (pdev_is_gen1(pdev) || pdev_is_gen3(pdev) ||
 	    pdev_is_gen4(pdev) || pdev_is_gen5(pdev))
 		xeon_deinit_dev(ndev);
