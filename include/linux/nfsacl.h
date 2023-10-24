9ee8f4e457bf28bb9504282dc7c61d2e61cc2656
Brian Meagher <brian.meagher@ixsystems.com>
2023-10-24 13:33:04 -0700

    Move some nfs4_acl_xdr handling functions from nfsd to nfs_common for reuse
    Also now use ACE4SIZE and add a check wrt remaining in
    convert_nfs41xdr_to_nfs40_acl / convert_to_nfs40_ace

--- include/linux/nfsacl.h
+++ include/linux/nfsacl.h
@@ -11,6 +11,9 @@
 #include <linux/posix_acl.h>
 #include <linux/sunrpc/xdr.h>
 #include <uapi/linux/nfsacl.h>
+#if CONFIG_TRUENAS
+#include <linux/nfs4.h>		/* For struct nfs4_acl */
+#endif /* CONFIG_TRUENAS */
 
 /* Maximum number of ACL entries over NFS */
 #define NFS_ACL_MAX_ENTRIES	1024
@@ -44,5 +47,12 @@ nfs_stream_decode_acl(struct xdr_stream *xdr, unsigned int *aclcnt,
 extern bool
 nfs_stream_encode_acl(struct xdr_stream *xdr, struct inode *inode,
 		      struct posix_acl *acl, int encode_entries, int typeflag);
+
+#if CONFIG_TRUENAS
+extern int
+convert_nfs41xdr_to_nfs40_acl(u32 *xdrbuf, size_t remaining, struct nfs4_acl *acl);
+extern int
+generate_nfs41acl_buf(u32 *xdrbuf, const struct nfs4_acl *acl);
+#endif /* CONFIG_TRUENAS */
 
 #endif  /* __LINUX_NFSACL_H */
