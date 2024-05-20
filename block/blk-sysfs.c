bdce457a0b130175847fd272e8c1c22d3a54d639
Nitesh Shetty <nj.shetty@samsung.com>
2024-05-20 15:50:14 +0530

    block: Introduce queue limits and sysfs for copy-offload support
    Add device limits as sysfs entries,
    	- copy_max_bytes (RW)
    	- copy_max_hw_bytes (RO)
    
    Above limits help to split the copy payload in block layer.
    copy_max_bytes: maximum total length of copy in single payload.
    copy_max_hw_bytes: Reflects the device supported maximum limit.
    
    Signed-off-by: Nitesh Shetty <nj.shetty@samsung.com>
    Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>
    Reviewed-by: Hannes Reinecke <hare@suse.de>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -245,6 +245,46 @@ static int queue_max_discard_sectors_store(struct gendisk *disk,
 	return 0;
 }
 
+static ssize_t queue_copy_hw_max_show(struct gendisk *disk, char *page)
+{
+	return sprintf(page, "%llu\n", (unsigned long long)
+		       disk->queue->limits.max_copy_hw_sectors << SECTOR_SHIFT);
+}
+
+static ssize_t queue_copy_max_show(struct gendisk *disk, char *page)
+{
+	return sprintf(page, "%llu\n", (unsigned long long)
+		       disk->queue->limits.max_copy_sectors << SECTOR_SHIFT);
+}
+
+static ssize_t queue_copy_max_store(struct gendisk *disk, const char *page,
+				    size_t count)
+{
+	unsigned long max_copy_bytes;
+	struct queue_limits lim;
+	ssize_t ret;
+	int err;
+	struct request_queue *q = disk->queue;
+	unsigned int memflags;
+
+	ret = queue_var_store(&max_copy_bytes, page, count);
+	if (ret < 0)
+		return ret;
+
+	if (max_copy_bytes & (queue_logical_block_size(q) - 1))
+		return -EINVAL;
+
+	memflags = blk_mq_freeze_queue(q);
+	lim = queue_limits_start_update(q);
+	lim.max_user_copy_sectors = max_copy_bytes >> SECTOR_SHIFT;
+	err = queue_limits_commit_update(q, &lim);
+	blk_mq_unfreeze_queue(q, memflags);
+
+	if (err)
+		return err;
+	return count;
+}
+
 static int queue_max_wzeroes_unmap_sectors_store(struct gendisk *disk,
 		const char *page, size_t count, struct queue_limits *lim)
 {
@@ -584,6 +624,9 @@ QUEUE_RO_ENTRY(queue_nr_zones, "nr_zones");
 QUEUE_LIM_RO_ENTRY(queue_max_open_zones, "max_open_zones");
 QUEUE_LIM_RO_ENTRY(queue_max_active_zones, "max_active_zones");
 
+QUEUE_RO_ENTRY(queue_copy_hw_max, "copy_max_hw_bytes");
+QUEUE_RW_ENTRY(queue_copy_max, "copy_max_bytes");
+
 QUEUE_RW_ENTRY(queue_nomerges, "nomerges");
 QUEUE_LIM_RW_ENTRY(queue_iostats_passthrough, "iostats_passthrough");
 QUEUE_RW_ENTRY(queue_rq_affinity, "rq_affinity");
@@ -719,6 +762,8 @@ static struct attribute *queue_attrs[] = {
 	&queue_discard_granularity_entry.attr,
 	&queue_max_discard_sectors_entry.attr,
 	&queue_max_hw_discard_sectors_entry.attr,
+	&queue_copy_hw_max_entry.attr,
+	&queue_copy_max_entry.attr,
 	&queue_atomic_write_max_sectors_entry.attr,
 	&queue_atomic_write_boundary_sectors_entry.attr,
 	&queue_atomic_write_unit_min_entry.attr,
