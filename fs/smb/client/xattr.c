632736a739c98a0c3ba47d1b623a4a8a18a18027
Ameer Hamza <ahamza@ixsystems.com>
2025-12-09 03:59:56 +0500

    Revert "cifs: Fix getting DACL-only xattr system.cifs_acl and system.smb3_acl"
    This reverts commit ad9364a6835c45c52f47587ffbe0577bb7cd4c5b.

--- fs/smb/client/xattr.c
+++ fs/smb/client/xattr.c
@@ -403,23 +403,10 @@ static int cifs_xattr_get(const struct xattr_handler *handler,
 		if (pTcon->ses->server->ops->get_acl == NULL)
 			goto out; /* rc already EOPNOTSUPP */
 
-		switch (handler->flags) {
-		case XATTR_CIFS_NTSD_FULL:
-			extra_info = OWNER_SECINFO | GROUP_SECINFO | DACL_SECINFO | SACL_SECINFO;
-			break;
-		case XATTR_CIFS_NTSD:
-			extra_info = OWNER_SECINFO | GROUP_SECINFO | DACL_SECINFO;
-			break;
-		case XATTR_CIFS_NTSD_OWNER:
-			extra_info = OWNER_SECINFO | GROUP_SECINFO;
-			break;
-		case XATTR_CIFS_NTSD_SACL:
+		if (handler->flags == XATTR_CIFS_NTSD_FULL) {
 			extra_info = SACL_SECINFO;
-			break;
-		case XATTR_CIFS_ACL:
-		default:
-			extra_info = DACL_SECINFO;
-			break;
+		} else {
+			extra_info = 0;
 		}
 		pacl = pTcon->ses->server->ops->get_acl(cifs_sb,
 				inode, full_path, &acllen, extra_info);
